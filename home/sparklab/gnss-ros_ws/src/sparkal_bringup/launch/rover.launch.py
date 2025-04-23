50675a340f21b7087f54d0fc57efb61b9192d43d
Toya Takahashi <toyatakahashi522@gmail.com>
2025-04-23 01:45:51 -0400

    finished launch file

--- home/sparklab/gnss-ros_ws/src/sparkal_bringup/launch/rover.launch.py
+++ home/sparklab/gnss-ros_ws/src/sparkal_bringup/launch/rover.launch.py
@@ -18,31 +18,60 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def launch_setup(context, *args, **kwargs):
 
+    microstrain_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                get_package_share_directory("microstrain_inertial_driver"),
+                "/launch/microstrain_launch.py",
+            ]
+        )
+    )
+
+    realsense_node = launch_ros.actions.Node(
+        package="realsense2_camera",
+        executable="realsense2_camera_node",
+        output="screen",
+    )
+
     ublox_ntrip_ld = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
                 get_package_share_directory("ntrip_client"),
                 "/ntrip_client_launch.py",
             ]
-        ),
+        )
+    )
+
+    vlp16_driver_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                get_package_share_directory("velodyne_driver"),
+                "/launch/velodyne_driver_node-VLP16-launch.py",
+            ]
+        )
+    )
+
+    vlp16_transform_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                get_package_share_directory("velodyne_pointcloud"),
+                "/launch/velodyne_transform_node-VLP16-launch.py",
+            ]
+        )
     )
 
     return [
+        microstrain_ld,
+        realsense_node,
         ublox_ntrip_ld,
+        vlp16_driver_ld,
+        vlp16_transform_ld,
     ]
 
 
 def generate_launch_description():
     return LaunchDescription(
         [
-            DeclareLaunchArgument("device", default_value="/dev/ttyACM0"),
-            DeclareLaunchArgument(
-                "host", default_value="macorsrtk.massdot.state.ma.us"
-            ),
-            DeclareLaunchArgument("port", default_value="10000"),
-            DeclareLaunchArgument("mountpoint", default_value="RTCM3_NEAR"),
-            DeclareLaunchArgument("username"),
-            DeclareLaunchArgument("password"),
             OpaqueFunction(function=launch_setup),
         ]
     )
